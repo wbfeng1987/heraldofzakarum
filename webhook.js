6347a29ccea512cd8e93a227d8e040a7f9ea7bd8
wbfeng1987 <wbfeng1987@gmail.com>
2020-05-11 14:33:21 +0800

    auto update with webhook revised2

--- webhook.js
+++ webhook.js
@@ -2,7 +2,7 @@ var http = require('http')
 var spawn = require('child_process').spawn
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({
-    path: '/',
+    path: '/auto_update',
     secret: 'b79e853b10a5fcf53606c1cb060fee85'    //这里是随机字符串，与 Github 上的配置保持一致
 })
 
